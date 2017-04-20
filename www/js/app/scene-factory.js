a067e0214bc55cd50a48cb121c1c1ef8386d47da
david.poemojo <david.levinson@poemojo.com>
2017-04-20 14:32:31 -0400

    Updated Body Text Display

--- www/js/app/scene-factory.js
+++ www/js/app/scene-factory.js
@@ -1,7 +1,7 @@
 (function(){
 var sceneFactory = angular.module('sceneFactory', []);
 
-sceneFactory.factory('StoryScene',  function($http)
+sceneFactory.factory('StoryScene',  ['$http', '$cookies', 'ExDate', function($http, $cookies, ExDate)
 {
    function isJson(json)
    {
@@ -21,7 +21,7 @@ sceneFactory.factory('StoryScene',  function($http)
          scene.title = response.title;
          scene.body = response.body;
          scene.image = response.img;
-         scene.coords = response.coords;
+         scene.items = response.items;
          scene.children = response.children;
          scene.message = response.msg;
       })
@@ -31,13 +31,49 @@ sceneFactory.factory('StoryScene',  function($http)
          scene.title = "Error!"
          scene.body = err;
          scene.image = null;
-         scene.coords = [];
+         scene.items = [];
          scene.children = [];
          scene.message = "";
 
       });
    }
 
+   StoryScene.prototype =
+   {
+      parent: null,
+      id: null,
+      message: "",
+      title: "",
+      body: [],
+      image: null,
+      items: [],
+      children:[],
+
+      save: function()
+      {
+         var scene = this;
+         var obj = {
+            id:       scene.id,
+            msg:      scene.message,
+            title:    scene.title,
+            body:     scene.body,
+            img:      scene.image,
+            items:    scene.items,
+            children: scene.children
+         };
+
+         try
+         {
+            var json = JSON.stringify(obj);
+            $cookies.put("aqan-tboj-scene", json, { 'expires': ExDate("1h").expire });
+         }
+         catch (e)
+         { return false; }
+
+         return true;
+      }
+
+   };
 
 
    return {
@@ -48,5 +84,5 @@ sceneFactory.factory('StoryScene',  function($http)
    };
 
 
-});
+}]);
 })();
